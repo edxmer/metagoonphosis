f6e4882cbf1714563eadbcdbe40edc5bcdffa5ff
Roland <srolo2007@gmail.com>
2026-06-17 15:30:38 +0200

    added_goon_sprites

--- components/organ/Organ.cs
+++ components/organ/Organ.cs
@@ -3,33 +3,33 @@ using System;
 
 public enum OrganType
 {
-    HEART,
-    LUNG,
-    OTHER
+	HEART,
+	LUNG,
+	OTHER
 }
 
 [GlobalClass]
 public partial class Organ : Node
 {
-    [Export]
-    public OrganType Type { get; set; } = OrganType.OTHER;
+	[Export]
+	public OrganType Type { get; set; } = OrganType.OTHER;
 
-    // Top left position
-    [Export]
-    public int TopLeftPositionX { get; set; }
-    [Export]
-    public int TopLeftPositionY { get; set; }
+	// Top left position
+	[Export]
+	public int TopLeftPositionX { get; set; }
+	[Export]
+	public int TopLeftPositionY { get; set; }
 
-    [Export(PropertyHint.MultilineText)]
-    public string OrganShapeString { get; set; }
+	[Export(PropertyHint.MultilineText)]
+	public string OrganShapeString { get; set; }
 
-    public bool[,] Shape;
+	public bool[,] Shape;
 
-    public int Width, Height;
+	public int Width, Height;
 
-    public override void _Ready()
-    {
-        (Width, Height) = ShapeStringHelper.GetDimensions(OrganShapeString);
-        Shape = ShapeStringHelper.ParseShape(OrganShapeString, Width, Height);
-    }
+	public override void _Ready()
+	{
+		(Width, Height) = ShapeStringHelper.GetDimensions(OrganShapeString);
+		Shape = ShapeStringHelper.ParseShape(OrganShapeString, Width, Height);
+	}
 }
