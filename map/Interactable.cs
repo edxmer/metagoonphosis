c74f80fb9a6bee8bd1f6df3e47db1f0dc23f63c0
Roland <srolo2007@gmail.com>
2026-06-18 17:01:55 +0200

    do interactable base, adn fonts

--- map/Interactable.cs
+++ map/Interactable.cs
@@ -6,28 +6,48 @@ public partial class Interactable : Area2D
 	// Called when the node enters the scene tree for the first time.
 	private AnimatedSprite2D animation;
 	private Area2D playerLookHitbox;
+	
+	private bool amISeen=false;
 	public override void _Ready()
 	{
+		AreaEntered += OnAreaEntered;
+		AreaExited += OnAreaExited;
 		animation=GetNode<AnimatedSprite2D>("Sprite");
-		var player = GetTree().GetFirstNodeInGroup("player");
+		var player = GetTree().GetFirstNodeInGroup("palayer");
 		if (player != null)
 		{
 			playerLookHitbox = player.GetNode<Area2D>("LookingAtHitbox");
 		}
 	}
+	private void OnAreaEntered(Area2D area)
+	{
+		if (area.IsInGroup("playerLookHitbox"))
+		{
 
+			amISeen=true;
+		}
+	}
+	private void OnAreaExited(Area2D area)
+	{
+		if (area.IsInGroup("playerLookHitbox"))
+		{
+			amISeen=false;
+		}
+	}
+	public virtual void ActionOnInteract()
+	{
+			var pos=Position;
+			pos.X+=5;
+			Position=pos;
+	}
+	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (Input.IsActionJustPressed("ui_accept"))
+		
+		if (amISeen &&Input.IsActionJustPressed("ui_accept"))
 		{
-			var overlapping=GetOverlappingBodies();
-			if (overlapping.Contains(playerLookHitbox))
-			{
-				Vector2 pos=Position;
-				pos.X+=5;
-				Position=pos;
-			}
+			ActionOnInteract();
 			
 		}
 	}
