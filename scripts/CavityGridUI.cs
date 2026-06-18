1967f0ca1c18dd83c88cfcfda719a0fa951a07e3
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-06-18 17:51:00 +0200

    Center cavity UI

--- scripts/CavityGridUI.cs
+++ scripts/CavityGridUI.cs
@@ -8,11 +8,13 @@ public partial class CavityGridUI : Control
 	[Export] public int SlotSizePx { get; set; } = 64;
 
 	private ChestCavity _chestCavity;
+	private Vector2 PositionOffset = Vector2.Zero; // Offset to centre the draw functions
 
 
 	public void Initialize(ChestCavity cavity)
 	{
 		_chestCavity = cavity;
+		PositionOffset = new Vector2(-cavity.Width * SlotSizePx * 0.5f, -cavity.Height * SlotSizePx * 0.5f);
 		ReloadVisualItems();
 	}
 
@@ -23,10 +25,10 @@ public partial class CavityGridUI : Control
         for (int x=0; x<_chestCavity.Width; ++x) for (int y=0; y<_chestCavity.Height; ++y)
 		if (_chestCavity.Shape[x, y])
 		{
-			Vector2 topLeftCorner = new(x*SlotSizePx, y*SlotSizePx);
-			Vector2 topRightCorner = new((x+1)*SlotSizePx, y*SlotSizePx);
-			Vector2 botLeftCorner = new(x*SlotSizePx, (y+1)*SlotSizePx);
-			Vector2 botRightCorner = new((x+1)*SlotSizePx, (y+1)*SlotSizePx);
+			Vector2 topLeftCorner = new Vector2(x*SlotSizePx, y*SlotSizePx) + PositionOffset;
+			Vector2 topRightCorner = new Vector2((x+1)*SlotSizePx, y*SlotSizePx) + PositionOffset;
+			Vector2 botLeftCorner = new Vector2(x*SlotSizePx, (y+1)*SlotSizePx) + PositionOffset;
+			Vector2 botRightCorner = new Vector2((x+1)*SlotSizePx, (y+1)*SlotSizePx) + PositionOffset;
 
 			DrawLine(topLeftCorner, topRightCorner, lineColor);
 			DrawLine(topRightCorner, botRightCorner, lineColor);
@@ -34,6 +36,8 @@ public partial class CavityGridUI : Control
 			DrawLine(botLeftCorner, topLeftCorner, lineColor);
 		}
     }
+
+
 
 	public void ReloadVisualItems()
     {
@@ -57,7 +61,7 @@ public partial class CavityGridUI : Control
 		var view = new OrganViewUI();
 		view.Initialize(slot, SlotSizePx);
 
-		view.Position = new Vector2(origin.X * SlotSizePx, origin.Y * SlotSizePx);
+		view.Position = new Vector2(origin.X * SlotSizePx, origin.Y * SlotSizePx) + PositionOffset;
 
 		AddChild(view);
 	}
