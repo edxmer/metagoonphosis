4e9258ab773046c0c2bd2241215d26ed02f6c744
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-06-18 16:06:03 +0200

    Modify chest cavity to work to work well with the new system

--- scripts/ChestCavity.cs
+++ scripts/ChestCavity.cs
@@ -3,13 +3,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
+#pragma warning disable CS0649
+
 [GlobalClass]
 public partial class ChestCavity : Resource
 {
+	[Signal]
+	public delegate void OrgansChangedEventHandler();
+
 	/// <summary>
 	/// O for available position,
 	/// X (or anything else) for not available position.
-	/// 
 	/// If line is cut off early, it will
 	/// consider the rest of it not available.
 	/// </summary>
@@ -20,48 +24,77 @@ public partial class ChestCavity : Resource
 		set
 		{
 			_cavityShapeString = value;
-			(_width, _height) = ShapeStringHelper.GetDimensions(value);
-			_shape = ShapeStringHelper.ParseShape(value, _width, _height);
+			if (string.IsNullOrEmpty(_cavityShapeString)) return;
+			(Width, Height) = ShapeStringHelper.GetDimensions(value);
+			GD.Print($"Cavity width: {Width}, height: {Height}");
+			Shape = ShapeStringHelper.ParseShape(value, Width, Height);
+			_items = new CavitySlot[Width, Height];
 		}
 	}
 
-	public List<Organ> Organs;
+	public int Width, Height;
+	public bool[,] Shape;
 
 	private string _cavityShapeString;
-	private int _width, _height;
-	private bool[,] _shape;
+
 	private CavitySlot[,] _items;
+	private Dictionary<CavitySlot, Vector2I> _itemOrigins = new();
 
-	public void LoadOrgans()
+	public bool CanPlaceOrgan(Organ organ, Vector2I origin)
 	{
-		// TODO: finish this
-	}
-
-	public bool DoesFit(Organ organ, int posI, int posJ)
-	{
-		for (int i = 0; i<organ.Height; ++i)
+		if (origin.X < 0 || origin.Y < 0 || Width < origin.X + organ.Width || Height < origin.Y + organ.Height)
 		{
-			for (int j = 0; j<organ.Width; ++j)
+			return false;
+		}
+
+		for (int x=0; x<organ.Width; ++x) for (int y=0; y<organ.Height; ++y) 
+		{
+			if (organ.Shape[x, y])
 			{
-				if 
-				(
-					organ.Shape[i, j] && 
-					!(
-						(i+posI < _height) &&
-						(j+posJ < _width)  &&
-						!_shape[i+posI, j+posJ]
-					)
-				) return false;
+				if (!Shape[origin.X + x, origin.Y + y] || _items[origin.X + x, origin.Y + y] != null ) return false;
 			}
 		}
 
 		return true;
 	}
 
-	public void AddOrgan(Organ organ, int posI, int posJ)
+	public bool TryPlaceOrgan(Organ organ, Vector2I origin)
 	{
+		if (!CanPlaceOrgan(organ, origin)) return false;
+
+		CavitySlot slot = new CavitySlot(organ);
+		_itemOrigins.Add(slot, origin);
 		
+
+		for (int x=0; x<organ.Width; ++x) for (int y=0; y<organ.Height; ++y) 
+		{
+			if (organ.Shape[x, y])
+			{
+				_items[origin.X + x, origin.Y + y] = slot;
+			}
+		}
+
+		return true;
 	}
 
-	
+	public void RemoveSlot(CavitySlot slot)
+	{
+		Vector2I origin = _itemOrigins[slot];
+		Organ organ = slot.Organ;
+
+		_itemOrigins.Remove(slot);
+
+		for (int x=0; x<organ.Width; ++x) for (int y=0; y<organ.Height; ++y) 
+		{
+			if (organ.Shape[x, y])
+			{
+				_items[origin.X + x, origin.Y + y] = null;
+			}
+		}
+	}
+
+	public List<Organ> GetOrgans() => _itemOrigins.Keys.Select(x => x.Organ).ToList();
+	public List<KeyValuePair<CavitySlot, Vector2I>> GetItems() => _itemOrigins.ToList();
 }
+
+#pragma warning restore CS0649
