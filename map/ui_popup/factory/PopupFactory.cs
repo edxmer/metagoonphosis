4379e8a3b6a2e756a8b649fc8f38f6afd12e2191
Roland <srolo2007@gmail.com>
2026-06-21 20:32:55 +0200

    popup fight done?

--- map/ui_popup/factory/PopupFactory.cs
+++ map/ui_popup/factory/PopupFactory.cs
@@ -5,6 +5,7 @@ public static class PopupFactory
 {
 		private static PackedScene popupItemScene = GD.Load<PackedScene>("res://map/ui_popup/item/buy_something_popup_item.tscn");
 	private static PackedScene popupOrganScene = GD.Load<PackedScene>("res://map/ui_popup/organ/buy_something_popup_organ.tscn");
+	private static PackedScene popupFightScene = GD.Load<PackedScene>("res://map/ui_popup/fight/buy_something_popup_fight.tscn");
 	// Called when the node enters the scene tree for the first time.
 	public static BuySomethingPopup CreatePopupItem(Node2D parent,int cost,string rewardItem,int rewardAmount)
 	{
@@ -13,6 +14,20 @@ public static class PopupFactory
 		item.rewardItem=rewardItem;
 		item.rewardAmount=rewardAmount;
 		item.neededMoney=cost;
+		item.Position=new Vector2(parent.Position.X,parent.Position.Y+35);
+		item.setMyText();
+		return item;
+	}
+		public static BuySomethingPopup CreatePopupFight(Node2D parent,EnemyData enemyData,string text,int rewardMoney,Organ? rewardOrgan)
+	{
+			
+		var item =  popupFightScene.Instantiate<BuySomethingPopupFight>();
+		parent.AddChild(item);
+		item.enemyData=enemyData;
+		item.rewardOrgan=rewardOrgan;
+		item.parent=parent;
+		item.rewardMoney=rewardMoney;
+		item.neededMoney=0;
 		item.Position=new Vector2(parent.Position.X,parent.Position.Y+35);
 		item.setMyText();
 		return item;
