f3a1bd7a0396bd0bcb4142444db600c4702e45de
Nagy Edömér Tamás <bigedomer@gmail.com>
2026-06-21 12:58:46 +0200

    Create Ability base class

---
+++ scripts/fight/Ability.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class Ability : Resource
+{
+    [Export] public string AbilityName { get; set; }
+    [Export] public string AbilityDescription { get; set; }
+
+    [Export] public Texture2D AttackTexture { get; set; }
+
+    [Export] public int DefaultCooldown { get; set; } = 1;
+
+    public int Cooldown { get; private set; } = 0;
+
+
+    public virtual void CastAbility(FightMemberState from, FightMemberState to)
+    {
+        Cooldown = DefaultCooldown;
+    }
+
+    public void ResetCooldown()
+    {
+        Cooldown = 0;
+    }
+
+    public void TickCooldown()
+    {
+        Cooldown = Math.Max(0, Cooldown - 1);
+    }
+}
